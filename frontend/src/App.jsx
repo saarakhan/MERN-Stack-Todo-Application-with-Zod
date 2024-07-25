688626eca52666d435c3513ee9d0b427e9b2b9d3
Saara Khan <khansarah0716@gmail.com>
2024-07-25 20:03:02 +0530

    Adding Frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import { CreateTodo } from "./components/CreateTodo";
+import "./App.css";
+
+function App() {
+
+  return (
+    <div>
+      <CreateTodo />
+    </div>
+  );
+}
+
+export default App;
+ 
