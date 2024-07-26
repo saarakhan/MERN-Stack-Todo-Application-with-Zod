0a05f2c110801ec2e7a8e38100c5cca0476aecd6
Saara Khan <khansarah0716@gmail.com>
2024-07-26 15:29:58 +0530

    fix issue

--- Backend/db.js
+++ Backend/db.js
@@ -10,12 +10,12 @@ const mongoose = require("mongoose");
 
 mongoose.connect("mongodb://localhost:27017");
 const todoSchema = mongoose.Schema({
-    title: String,
-    description: String,
-    completed: Boolean
+  title: String,
+  description: String,
+  completed: Boolean
 })
 
 const todo = mongoose.model('todo', todoSchema);
 module.exports = {
-    todo
+  todo
 }
